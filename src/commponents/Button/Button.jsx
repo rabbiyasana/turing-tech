6920864af052c9e018646a922327bf374c89a51e
Rabbia sana <rabbiasana55@gmail.com>
2023-07-11 01:33:17 +0500

    add logout button component and add in navbar

---
+++ src/commponents/Button/Button.jsx
@@ -0,0 +1,9 @@
+function Button(props) {
+  return (
+    <>
+      <button style={{ backgroundColor: "#4F46F8" }}>{props.text}</button>
+    </>
+  );
+}
+
+export default Button;
--- src/commponents/navbar/navbar.jsx
+++ src/commponents/navbar/navbar.jsx
@@ -1,16 +1,18 @@
 import ttLogo from "../../assets/images/tt-logo.png";
+import Button from "../Button/button";
 function Navbar() {
   return (
     <>
-      <div className="container">
+      <div className="container" style={{ height: "80px" }}>
         <div className="row">
-          <div className="col-sm-4">
-            <img src={ttLogo} alt="turing-logo" className="mw-30" />
+          <div className="col-sm-6">
+            <img src={ttLogo} alt="turing-logo" style={{ width: "313px" }} />
           </div>
-          <div className="col-sm-8"></div>
+          <div className="col-sm-6">
+            <Button text="Logout" />
+          </div>
         </div>
       </div>
-      <p>navbar</p>
     </>
   );
 }
