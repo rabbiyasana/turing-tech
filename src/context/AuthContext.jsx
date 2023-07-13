20297f2959bbdd04875658011cd12b385a596f8e
Rabbia sana <rabbiasana55@gmail.com>
2023-07-13 23:21:40 +0500

    define logout functionin AuthWrapper

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,11 +1,14 @@
 import { createContext, useContext, useState } from "react";
 import axios from "axios";
 import { baseUrl } from "../utilities/api";
+import { AuthHeader } from "../utilities/header";
 const AuthContext = createContext();
 
 export function AuthProvider(props) {
-  const [loggedIn, setLoggedIn] = useState(!!props.logged);
-  console.log(props.logged);
+  // const [loggedIn, setLoggedIn] = useState(!!props.logged);
+  // console.log(props.logged);
+  const [user, setUser] = useState("");
+  // login function
   const Login = async (username, password) => {
     const res = await axios({
       method: "post",
@@ -17,14 +20,24 @@ export function AuthProvider(props) {
     });
     console.log(res.data);
     const { user, access_token, refresh_token } = res.data;
+
+    // the user from res use it to manag the state
+    setUser(user);
     localStorage.setItem("access_token", access_token);
     localStorage.setItem("refresh_token", refresh_token);
     localStorage.setItem("user", user.id);
   };
 
+  // logout function
+
+  const Logout = () => {
+    setUser("");
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("refresh_token");
+  };
   return (
     <>
-      <AuthContext.Provider value={{ loggedIn, setLoggedIn, Login }}>
+      <AuthContext.Provider value={{ setLoggedIn, Login }}>
         {props.children}
       </AuthContext.Provider>
     </>
